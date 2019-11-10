4d074f910aa2f7ce1e1f5e92b3a430e401b1c5df
YuSJ <joelyusj@hotmail.com>
2019-11-10 22:44:09 +0800

    add IP validation

---
+++ CodeWars/IpValidation.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class IpValidation
+    {
+        /// <summary>
+        /// My Solution
+        /// </summary>
+        /// <param name="ipAddress"></param>
+        /// <returns></returns>
+        public static bool is_valid_IP(string ipAddress)
+        {
+            if (string.IsNullOrEmpty(ipAddress))
+                return false;
+
+            var substrings = ipAddress.Split('.');
+            if (substrings.Length != 4)
+                return false;
+
+            foreach (string substring in substrings)
+            {
+                int i;
+                if (!int.TryParse(substring, out i))
+                    return false;
+
+                if (i > 255 || i < 0)
+                    return false;
+
+                if (substring != i.ToString())
+                    return false;
+            }
+
+            return true;
+        }
+        
+        public static bool is_valid_IP2(string ipAddress)
+        {
+            // 这个解可能最符合题意
+            string regexPattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+            return Regex.IsMatch(ipAddress, regexPattern);
+        }
+
+        public static bool is_valid_IP3(string ipAddress)
+        {
+            // 其实我的代码也不比这个好到哪里
+            bool validIp = IPAddress.TryParse(ipAddress, out var ip);
+            return validIp && ip.ToString() == ipAddress;
+        }
+    }
+}
---
+++ CodeWarsTests/IpValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IpValidationTests
+    {
+        [Test]
+        public void TestCases()
+        {
+            Assert.AreEqual(true, IpValidation.is_valid_IP("0.0.0.0"));
+            Assert.AreEqual(true, IpValidation.is_valid_IP("12.255.56.1"));
+            Assert.AreEqual(true, IpValidation.is_valid_IP("137.255.156.100"));
+
+            Assert.AreEqual(false, IpValidation.is_valid_IP(""));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("abc.def.ghi.jkl"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("123.456.789.0"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56.00"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56.7.8"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.256.78"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("1234.34.56"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("pr12.34.56.78"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56.78sf"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56 .1"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("12.34.56.-1"));
+            Assert.AreEqual(false, IpValidation.is_valid_IP("123.045.067.089"));
+        }
+    }
+}
