571d0ec1a6c5a3a15ec29ca7d81f39ab16c24859
yusjoel <joelyusj@hotmail.com>
2019-11-13 18:29:49 +0800

    Find the odd int

---
+++ CodeWars/Kyu6/Kata002.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars.Kyu6
+{
+    public class Kata002
+    {
+        /// <summary>
+        ///     My Solution
+        /// </summary>
+        /// <param name="seq"></param>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        public static int find_it(int[] seq)
+        {
+            if (seq == null)
+                throw new ArgumentNullException(nameof(seq));
+
+            var result = new List<int>();
+
+            foreach (int i in seq)
+                if (result.Contains(i))
+                    result.Remove(i);
+                else
+                    result.Add(i);
+
+            if (result.Count == 1)
+                return result[0];
+
+            throw new ArgumentException("wrong sequence");
+        }
+
+        /// <summary>
+        ///     Clever
+        /// </summary>
+        /// <param name="seq"></param>
+        /// <returns></returns>
+        public static int find_it2(int[] seq)
+        {
+            int found = 0;
+
+            foreach (int num in seq)
+                found ^= num;
+
+            return found;
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu6/Kata002Tests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars.Kyu6;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Kyu6
+{
+    [TestFixture()]
+    public class Kata002Tests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(5, Kata002.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
