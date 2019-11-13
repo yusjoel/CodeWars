d7194a6c1a3be5c95031b315b7f0f97acd7e4498
yusjoel <joelyusj@hotmail.com>
2019-11-13 10:27:56 +0800

    新增: Sums of Parts

---
+++ CodeWars/Kyu6/SumParts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodeWars.Kyu6
+{
+    /// <summary>
+    ///     Sums of Parts
+    /// </summary>
+    /// <remarks>
+    ///     https://www.codewars.com/kata/5ce399e0047a45001c853c2b
+    /// </remarks>
+    public class SumParts
+    {
+        public static int[] PartsSums(int[] ls)
+        {
+            if (ls == null)
+                throw new ArgumentNullException(nameof(ls));
+
+            int length = ls.Length;
+            var sums = new int[length + 1];
+
+            checked
+            {
+                try
+                {
+                    int sum = 0;
+                    sums[length] = 0;
+                    for (int i = length - 1; i >= 0; i--)
+                    {
+                        sum += ls[i];
+                        sums[i] = sum;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+            return sums;
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu6/SumPartsTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeWars.Kyu6;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsTests.Kyu6
+{
+    [TestFixture]
+    public class SumPartsTests
+    {
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once IdentifierTypo
+        private static void dotest(int[] ls, int[] exp)
+        {
+            var ans = SumParts.PartsSums(ls);
+            Assert.AreEqual(exp, ans);
+        }
+
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once IdentifierTypo
+        public static void atest0()
+        {
+            Console.WriteLine("Basic Tests");
+            dotest(new int[] { }, new[] { 0 });
+            dotest(new[] { 0, 1, 3, 6, 10 }, new[] { 20, 20, 19, 16, 10, 0 });
+            dotest(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 21, 20, 18, 15, 11, 6, 0 });
+            dotest(new[] { 744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358 },
+                new[]
+                {
+                    10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358,
+                    0
+                });
+        }
+    }
+}
