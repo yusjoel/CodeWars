a22accf95bc756db33ff1aee2fb8231329913c94
yusjoel <joelyusj@hotmail.com>
2019-11-25 18:03:54 +0800

    [新增] Persistent Bugger

---
+++ CodeWars/Kyu6/Persist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CodeWars.Kyu6
+{
+    /// <summary>
+    ///     Persistent Bugger.
+    /// </summary>
+    /// <remarks>
+    ///     https://www.codewars.com/kata/persistent-bugger/train/csharp
+    /// </remarks>
+    public class Persist
+    {
+        public static int Persistence(long n)
+        {
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(nameof(n));
+
+            long i = n;
+            long j = 1;
+            int times = 0;
+            while (i >= 10)
+            {
+                while (i > 0)
+                {
+                    j *= i % 10;
+                    i /= 10;
+                }
+                i = j;
+                j = 1;
+                times++;
+            }
+            return times;
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu6/PersistTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars.Kyu6;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsTests.Kyu6
+{
+    [TestFixture]
+    public class PersistTests
+    {
+        [Test]
+        public void PersistenceTest()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(3, Persist.Persistence(39));
+            Assert.AreEqual(0, Persist.Persistence(4));
+            Assert.AreEqual(2, Persist.Persistence(25));
+            Assert.AreEqual(4, Persist.Persistence(999));
+        }
+    }
+}
