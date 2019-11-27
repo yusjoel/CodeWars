929a4e4e9485383505a748395f64d3c9b9c02865
yusjoel <joelyusj@hotmail.com>
2019-11-27 11:40:00 +0800

    [新增]Rot13

---
+++ CodeWars/Kyu5/CaesarCipher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWars.Kyu5
+{
+    /// <summary>
+    ///     Rot13
+    /// </summary>
+    /// <remarks>
+    ///     https://www.codewars.com/kata/530e15517bc88ac656000716/train/csharp
+    /// </remarks>
+    public class CaesarCipher
+    {
+        public static string Rot13(string message)
+        {
+            if(message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            var stringBuilder = new StringBuilder();
+            foreach (char c in message)
+                if (char.IsLetter(c))
+                {
+                    bool isLower = char.IsLower(c);
+                    char a = isLower ? 'a' : 'A';
+                    char z = isLower ? 'z' : 'Z';
+                    char ch = (char) (c + 13);
+                    if (ch > z)
+                        ch = (char) (a + ch - z - 1);
+
+                    stringBuilder.Append(ch);
+                }
+                else
+                {
+                    stringBuilder.Append(c);
+                }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu5/CaesarCipherTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars.Kyu5;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Kyu5
+{
+    [TestFixture]
+    public class CaesarCipherTests
+    {
+        [Test]
+        [Description("test")]
+        // ReSharper disable once InconsistentNaming
+        public void testTest()
+        {
+            Assert.AreEqual("grfg", CaesarCipher.Rot13("test"),
+                string.Format("Input: test, Expected Output: grfg, Actual Output: {0}", CaesarCipher.Rot13("test")));
+        }
+
+        [Test]
+        [Description("Test")]
+        public void TestTest()
+        {
+            Assert.AreEqual("Grfg", CaesarCipher.Rot13("Test"),
+                string.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", CaesarCipher.Rot13("Test")));
+        }
+    }
+}
