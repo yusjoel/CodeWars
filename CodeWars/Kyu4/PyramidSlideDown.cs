e30d4d1f8cc548d1b821a8e27a8af3f55ade5cce
yusjoel <joelyusj@hotmail.com>
2019-11-28 18:09:23 +0800

    [优化]Pyramid Slide Down

--- CodeWars/Kyu4/PyramidSlideDown.cs
+++ CodeWars/Kyu4/PyramidSlideDown.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace CodeWars.Kyu4
 {
@@ -26,32 +25,15 @@ namespace CodeWars.Kyu4
                     throw new ArgumentException($"pyramid[{i}].Length is no {i + 1}");
             }
 
-            // 每个点都有两种可能, 上方的左路或者右路下来
-            // 只要取两者之中的最大值即可
-            // 考虑到不希望改动原始数据, 所以创建两个数组来记录
-            // previous记录上一层各个节点的最大值, 为了不要判断出界的情况, 大小为n+1
-            // 其中[0]和[n]是不用的
-            // current记录本层的各个节点的最大值, 计算完毕后再覆盖到previous中
+            // 对于第i层的节点j, 总是选择第i+1层的节点j与节点j+1中较大的一个
+            // 从底部向上按此规则计算, 到顶部后即为解
 
-            // [0, 1-n-1, n]
-            var previous = new int[n + 1];
-            // [0-n]
-            var current = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                var level = pyramid[i];
-                for (int j = 0; j < i + 1; j++)
-                {
-                    int left = previous[j] + level[j];
-                    int right = previous[j + 1] + level[j];
-                    current[j] = left > right ? left : right;
-                }
-                for (int j = 0; j < i + 1; j++)
-                    previous[j + 1] = current[j];
-            }
+            var floor = new int[n + 1];
+            for (int i = n - 1; i >= 0; i--)
+            for (int j = 0; j < i + 1; j++)
+                floor[j] = pyramid[i][j] + (floor[j] > floor[j + 1] ? floor[j] : floor[j + 1]);
 
-            int max = current.Max();
-            return max;
+            return floor[0];
         }
     }
 }
