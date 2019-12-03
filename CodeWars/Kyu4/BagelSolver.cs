5adf10e241a11ec5a7cdef59c901a70515f91508
yusjoel <joelyusj@hotmail.com>
2019-12-03 12:11:03 +0800

    [新增]Bagels

---
+++ CodeWars/Kyu4/BagelSolver.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars.Kyu4
+{
+    public sealed class Bagel
+    {
+        public int Value { get; } = 3;
+    }
+
+    /// <summary>
+    ///     Bagels
+    /// </summary>
+    /// <remarks>
+    ///     https://www.codewars.com/kata/bagels/train/csharp
+    /// </remarks>
+    public class BagelSolver
+    {
+        public static Bagel Bagel
+        {
+            get
+            {
+                var bagel = new Bagel();
+                var propertyInfo = typeof(Bagel).GetProperty(nameof(Bagel.Value));
+                if (propertyInfo != null)
+                    propertyInfo.SetValue(bagel, 4);
+
+                return bagel;
+            }
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu4/BagelSolverTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars.Kyu4;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Kyu4
+{
+    [TestFixture]
+    public class BagelSolverTests
+    {
+        [Test]
+        public void TestBagel()
+        {
+            var bagel = BagelSolver.Bagel;
+            Assert.AreEqual(4, bagel.Value);
+        }
+    }
+}
