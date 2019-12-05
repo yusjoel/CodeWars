a5665efc6d11919dd9cc859511429d7bd3b5b646
yusjoel <joelyusj@hotmail.com>
2019-12-05 11:07:17 +0800

    [新增]Factorial tail

---
+++ CodeWars/Kyu4/FactorialTail.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars.Kyu4
+{
+    /// <summary>
+    ///     Factorial tail
+    /// </summary>
+    /// <remarks>
+    ///     https://www.codewars.com/kata/factorial-tail/train/csharp
+    /// </remarks>
+    public class FactorialTail
+    {
+        public static int zeroes(int baseNumber, int number)
+        {
+            // 对于base, 先做因式分解, 如10 = 2x5, 16 = 2^4, 13 = 13
+            var baseNumberPrimeFactors = GetPrimeFactors(baseNumber);
+
+            // 然后计算2~number中包含多少个因子
+            int min = int.MaxValue;
+            foreach (var pair in baseNumberPrimeFactors)
+            {
+                int f = pair.Key;
+                int c = pair.Value;
+                int n = number / f;
+                int cnt = n;
+                while (n > 1)
+                {
+                    n = n / f;
+                    cnt += n;
+                }
+
+                cnt = cnt / c;
+                if (cnt < min)
+                    min = cnt;
+            }
+
+            return min;
+        }
+
+        private static Dictionary<int, int> GetPrimeFactors(int number)
+        {
+            var primes = new Dictionary<int, int>();
+
+            do
+            {
+                bool found = false;
+                int square = (int) Math.Sqrt(number);
+                for (int i = 2; i <= square; i++)
+                {
+                    if (number % i != 0)
+                        continue;
+
+                    if (!primes.ContainsKey(i))
+                        primes[i] = 1;
+                    else
+                        primes[i]++;
+                    number /= i;
+                    found = true;
+                    break;
+                }
+                if (!found)
+                {
+                    if (!primes.ContainsKey(number))
+                        primes[number] = 1;
+                    else
+                        primes[number]++;
+                    break;
+                }
+            } while (number > 1);
+            return primes;
+        }
+    }
+}
---
+++ CodeWarsTests/Kyu4/FactorialTailTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars.Kyu4;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Kyu4
+{
+    [TestFixture()]
+    public class FactorialTailTests
+    {
+        [Test]
+        public void Can_Be_Solved_With_Basic_Computations()
+        {
+            Assert.AreEqual(2, FactorialTail.zeroes(10, 10));
+            Assert.AreEqual(3, FactorialTail.zeroes(16, 16));
+        }
+    }
+}
